914a6bdf6925de110a83e155a1b38d9a94e66f0a
Rohit <rohitmandavkar3577@gmail.com>
2026-06-08 18:22:28 +0530

    written info

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Roboto } from "next/font/google";
+import { Inter, Roboto, Sirin_Stencil } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
@@ -13,9 +13,72 @@ const roboto = Roboto({
   subsets: ["latin"],
 });
 
+// My Info
+const siteConfig = {
+  name: "Rohit",
+  title: "Rohit | Web Engineer & Developer",
+  description:
+    "Web Engineer crafting digital art for the web. TypeScript and React are my go-to tools, backed by rigorous engineering.",
+  url: "https://rohitvince.in",
+  ogImage: "../public/profile.jpeg",
+  XHandle: "@rohitcpp",
+};
+
 export const metadata: Metadata = {
-  title: "Portfolio-Rohit",
-  description: "Portfolio, a digital crate for your stuff.",
+  metadataBase: new URL(siteConfig.url),
+
+  title: {
+    default: siteConfig.title,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "Web Developer",
+    "Frontend Engineer",
+    "React",
+    "Next.js",
+    "TypeScript",
+    "Tailwind CSS",
+    "Framer Motion",
+    "Portfolio",
+    "Rust Developer",
+    "Computer science",
+    siteConfig.name,
+  ],
+  authors: [{ name: siteConfig.name }],
+  creator: siteConfig.name,
+  icons: {
+    icon: "../public/favicon-32x32.png",
+    shortcut: "../public/favicon-32x32.png",
+    apple: "../public/favicon-32x32.png",
+  },
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.title,
+    description: siteConfig.description,
+    siteName: `${siteConfig.name}'s Portfolio`,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.title,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.title,
+    description: siteConfig.description,
+    creator: siteConfig.XHandle,
+    images: [siteConfig.ogImage],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
